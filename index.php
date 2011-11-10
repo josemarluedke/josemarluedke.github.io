4eaa0377336fc1387a50f26f12bbd5ba1bfbfa7e
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-10 11:20:43 -0200

    Index page created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Welcome - Josemar Davi Luedke | Web Developer</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="http://josemarluedke.com/Css/geral.css" />
+		<script type="text/javascript">
+			document.location.href = "http://josemarluedke.com";
+		</script>
+	</head>
+	<body>
+		Redirecting to JosemarLuedke.com
+	</body>
+</html>
