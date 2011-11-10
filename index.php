5e3cd1eac62c18a516f48916f2934dace4def555
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-10 11:28:19 -0200

    adjusting the css

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 	<head>
 		<title>Welcome - Josemar Davi Luedke | Web Developer</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="http://josemarluedke.com/Css/geral.css" />
+		<style type="text/css">
+			body {color:#FFFFFF; font:14px Arial, Helvetica, sans-serif; background:#222222; margin:0; padding:0px; text-align: center;}
+		</style>
 		<script type="text/javascript">
-			document.location.href = "http://josemarluedke.com";
+			//document.location.href = "http://josemarluedke.com";
 		</script>
 	</head>
 	<body>
